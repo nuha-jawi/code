7245cf291badcc60df5e541ccddfbef2c661f0a7
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-08-26 21:28:41 +0300

    Add files via upload

---
+++ Day7.py
@@ -0,0 +1,9 @@
+a= 'Hello'
+print(a)
+print("""You can be what ever you want
+trust in yourself . 
+and read a lot of books .
+and do good things for yourself XD """)
+b="Hello World"
+print(b[1])
+print(b[2:5])
