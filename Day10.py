9a6270a0f57a000e0887b69c266dadaaa6b1a801
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-08-29 19:07:04 +0300

    Add files via upload

---
+++ Day10.py
@@ -0,0 +1,17 @@
+x=5
+y=7
+print("X=",x)
+print("Y=",y)
+print("XxY=",x*y,"and X/Y= ",x/y)
+print("X+Y=",x+y,"and Y-X=",y-x)
+print("X%Y=",x%y)
+print("X**Y=",x**7)
+print("X//Y=",x//7,"\n")
+print("X>Y is",x>y)
+print("X<Y is",x<y)
+print("X>=Y is",x>=y)
+print("X<=Y is",x<=y)
+print("X==Y is",x==y)
+print("X!=Y is",x!=y)
+x += 3
+print(x)
