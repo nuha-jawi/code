19395a979bb1e04b14e99ac2fa14fbb80f1e36d4
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-01 21:36:52 +0300

    Add files via upload

---
+++ Day13.py
@@ -0,0 +1,11 @@
+s=[]
+print(s)
+numbers=[1,2,3,4,5,6]
+print(numbers)
+fruits = ['banana','cherry','apple']
+print(fruits)
+fn = ['banana','cherry','apple',1,2,3,4]
+print(fn[1])
+fn[1]='orange'
+print(fn)
+del fn[1]
