69638cfab18cd5eb4d3cb003423851e0cd8818d2
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-02 13:53:03 +0300

    Add files via upload

---
+++ Day14.py
@@ -0,0 +1,10 @@
+fruits=['banana','cherry','apple','orange']
+print(fruits[1:3])
+if 'orange' in fruits:
+    print('yes,orange in the fruits list')
+else:
+    print('no')
+fruits2=['pinapple','melon']
+fruits3=['watermelon']
+print(fruits3 *3)
+print(fruits3+fruits2)
