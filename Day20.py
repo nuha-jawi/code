39f146cc914903cf9f2d4801c5888a834cc08f90
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-08 18:51:31 +0300

    Add files via upload

---
+++ Day20.py
@@ -0,0 +1,13 @@
+s={}
+print(s)
+s2={'amani','sarah','nuha','heba','danah'}
+print(s2)
+s3={'amani','nuha',1,2,3,4,5}
+print(s3)
+print('amani' in s3)
+for x in s3:
+    print(x)
+s3.add('heba')
+print(s3)
+s3.update(['ahmed','rawan','yara'])
+print(s3)
