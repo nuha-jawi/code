f78f1db3fbc97ae4333b2545f3eeebfc4476be58
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-14 16:06:36 +0300

    Add files via upload

---
+++ Day25-26.py
@@ -0,0 +1,13 @@
+set={1,3,5,7,8}
+print(set)
+set.add(4)
+set.add(8)
+set.add(12)
+print(set)
+set.remove(8)
+print(set)
+d={'name':'pigeon','type':'bird','skin cover':'feathers'}
+print(d)
+print('the type is',d['type'])
+d['skin cover']='lether'
+print(d)
