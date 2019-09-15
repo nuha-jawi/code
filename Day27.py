6c8b9c94d60e4058f21e3076b8ed402c2f120dc5
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-15 18:41:43 +0300

    Add files via upload

---
+++ Day27.py
@@ -0,0 +1,24 @@
+a=50
+b=100
+c=100
+if b>a :
+    print('b is greater than a')
+if b<a :
+    print('b is not greater than a')
+elif b>a :
+    print('b is greater than a')
+else:
+    print('a = b')
+if b>a:print('B')
+if b>a and b==c:
+    print('BB')
+if b>a or b<a:
+    print('BBB')
+if b>20:
+    print('b is greater than 20')
+    if a>30:
+        print('a is greater than 30')
+    else:
+        print('B')
+else:
+    print('A')
