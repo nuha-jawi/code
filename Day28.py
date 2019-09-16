ed453427cb7c494f37898f4e3fd2c5662d5b1980
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-16 18:56:27 +0300

    Add files via upload

---
+++ Day28.py
@@ -0,0 +1,19 @@
+b=1
+while b<6:
+    print(b)
+    b+=1
+else: print('i is less than 6')
+i=1
+while i<6:
+    print(i)
+    if i==4:
+        break
+    i+=1
+
+a=0
+while a<6:
+    a+=1
+    if a==4:
+        continue
+    print(a)
+
