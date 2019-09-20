cb818da2d70d6df8cf7f5c5f11f19e777b8bbfb2
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-20 13:04:34 +0300

    Add files via upload

---
+++ Day31.py
@@ -0,0 +1,12 @@
+def func():
+    print('Hello World')
+func()
+
+def func2(fname):
+    print('My name is '+fname)
+func2('nuha')
+
+def func3(country='saudi arabia'):
+    print('I am from '+country)
+func3()
+func3('india')
