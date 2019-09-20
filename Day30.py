2b94509892829145507fc2911a29b62d6e6551fe
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-20 05:11:59 +0300

    Add files via upload

---
+++ Day30.py
@@ -0,0 +1,13 @@
+for x in range(6):
+    print(x)
+else:
+    print('Finally finished')
+for w in range(2,6):
+    print(w)
+for c in range(2,30,3):
+    print(c)
+f=['apple','orange','banana']
+v=['red','orange','yellow']
+for ff in f:
+    for vv in v:
+        print(ff,vv)
