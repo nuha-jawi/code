4a1e290cf012c491953e3a1f36813e3f2d0b976c
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-26 05:59:14 +0300

    Add files via upload

---
+++ Day38.py
@@ -0,0 +1,11 @@
+fruits=['apple','banana','pinapple']
+for x in fruits:
+    print(x)
+fruits.append('orange')
+print(fruits)
+fruits.pop(1)
+print(fruits)
+fruits.remove('orange')
+print(fruits)
+fruits.extend("cherry")
+print(fruits)
