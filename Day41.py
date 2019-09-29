8afb7f4905411c0d614f1908d2a9fb744f0206d9
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-09-29 17:09:52 +0300

    Add files via upload

---
+++ Day41.py
@@ -0,0 +1,10 @@
+class class2:
+    def __init__(self,age,name):
+        self.age=age
+        self.name=name
+    def func1(self):
+        print('my name is '+self.name)
+
+
+b=class2(20,'Nuha')
+b.func1()
