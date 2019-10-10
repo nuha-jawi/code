404fe416063d5a80b63649e0d44b9fe0a88f4c24
nuha-jawi <54371249+nuha-jawi@users.noreply.github.com>
2019-10-10 19:51:09 +0300

    Add files via upload

---
+++ Day52.py
@@ -0,0 +1,13 @@
+def func(name):
+    print('Hello, '+name)
+import Day1 as n
+func('Nuha')
+a=n.d['age']
+print(a)
+import platform
+print(platform.system())
+print(platform.python_version())
+print(dir(platform))
+print(dir(n))
+from Day1 import d
+print(d['name'])
